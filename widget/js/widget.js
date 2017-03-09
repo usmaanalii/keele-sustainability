ac5992f923f877b0d2ec6e5eb7788d4cd3c44389
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-09 13:53:46 +0000

    Add files via upload
    First draft for a widget based site, that allows users to add recycling measurements for materials.

---
+++ widget/js/widget.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(".button > .paper").click(function() {
+        $(".add-paper-info").slideToggle(400);
+    });
+
+    $(".button > .compost").click(function() {
+        $(".add-compost-info").slideToggle(400);
+    });
+
+    $(".button > .glass").click(function() {
+        $(".add-glass-info").slideToggle(400);
+    });
+
+    $(".button > .metal").click(function() {
+        $(".add-metal-info").slideToggle(400);
+    });
+
+    $(".button > .plastic").click(function() {
+        $(".add-plastic-info").slideToggle(400);
+    });
+
+    $(".button > .general").click(function() {
+        $(".add-general-info").slideToggle(400);
+    });
+});
