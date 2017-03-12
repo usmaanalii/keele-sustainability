37a3374d109b60b92d6e4c5150fea417efa813ec
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-12 18:18:05 +0000

    General cleanup

--- widget/js/widget.js
+++ widget/js/widget.js
@@ -6,21 +6,26 @@ $(document).ready(function() {
 
     $(".button > .compost").click(function() {
         $(".add-compost-info").slideToggle(400);
+        $(this).toggleClass('rotate'); // for button
     });
 
     $(".button > .glass").click(function() {
         $(".add-glass-info").slideToggle(400);
+        $(this).toggleClass('rotate'); // for button
     });
 
     $(".button > .metal").click(function() {
         $(".add-metal-info").slideToggle(400);
+        $(this).toggleClass('rotate'); // for button
     });
 
     $(".button > .plastic").click(function() {
         $(".add-plastic-info").slideToggle(400);
+        $(this).toggleClass('rotate'); // for button
     });
 
     $(".button > .general").click(function() {
         $(".add-general-info").slideToggle(400);
+        $(this).toggleClass('rotate'); // for button
     });
 });
