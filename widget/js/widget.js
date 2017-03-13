27cc50c0db0f5c5713c8a5e76d362c9c73af44a4
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-13 15:22:16 +0000

    - Added the correct colors to each button's '+' - Changed the .button class to .button-style for semantic reasons

--- widget/js/widget.js
+++ widget/js/widget.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-    $(".button > .paper").click(function() {
+    $(".button-style > .paper").click(function() {
         $(".add-paper-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button > .compost").click(function() {
+    $(".button-style > .compost").click(function() {
         $(".add-compost-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button > .glass").click(function() {
+    $(".button-style > .glass").click(function() {
         $(".add-glass-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button > .metal").click(function() {
+    $(".button-style > .metal").click(function() {
         $(".add-metal-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button > .plastic").click(function() {
+    $(".button-style > .plastic").click(function() {
         $(".add-plastic-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button > .general").click(function() {
+    $(".button-style > .general").click(function() {
         $(".add-general-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
