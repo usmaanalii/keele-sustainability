614b682cd41477eb38ac475bd04b1d01ac198e25
techy <jrdavenport85@gmail.com>
2017-03-21 18:33:25 +0000

    rework widget expand buttons with animations closes #5, closes #2

--- widget/js/widget.js
+++ widget/js/widget.js
@@ -1,31 +1,31 @@
 // TODO: Make the code more efficient, violates DRY
 $(document).ready(function() {
-    $(".button-style > .paper").click(function() {
+    $(".button-style.paper").click(function() {
         $(".add-paper-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button-style > .compost").click(function() {
+    $(".button-style.compost").click(function() {
         $(".add-compost-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button-style > .glass").click(function() {
+    $(".button-style.glass").click(function() {
         $(".add-glass-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button-style > .metal").click(function() {
+    $(".button-style.metal").click(function() {
         $(".add-metal-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button-style > .plastic").click(function() {
+    $(".button-style.plastic").click(function() {
         $(".add-plastic-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
 
-    $(".button-style > .general").click(function() {
+    $(".button-style.general").click(function() {
         $(".add-general-info").slideToggle(400);
         $(this).toggleClass('rotate'); // for button
     });
