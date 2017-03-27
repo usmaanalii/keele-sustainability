85cbfac47f86498cfcc9b64238282948271efbcb
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-27 15:05:25 +0100

    Added semantic naming for classes, solve issue #8

--- widget/js/widget.js
+++ widget/js/widget.js
@@ -1,32 +1,32 @@
 // TODO: Make the code more efficient, violates DRY
 $(document).ready(function() {
-    $(".button-style > .paper").click(function() {
+    $(".add-button-style > .paper").click(function() {
         $(".add-paper-info").slideToggle(400);
-        $(this).toggleClass('rotate'); // for button
+        $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".button-style > .compost").click(function() {
+    $(".add-button-style > .compost").click(function() {
         $(".add-compost-info").slideToggle(400);
-        $(this).toggleClass('rotate'); // for button
+        $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".button-style > .glass").click(function() {
+    $(".add-button-style > .glass").click(function() {
         $(".add-glass-info").slideToggle(400);
-        $(this).toggleClass('rotate'); // for button
+        $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".button-style > .metal").click(function() {
+    $(".add-button-style > .metal").click(function() {
         $(".add-metal-info").slideToggle(400);
-        $(this).toggleClass('rotate'); // for button
+        $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".button-style > .plastic").click(function() {
+    $(".add-button-style > .plastic").click(function() {
         $(".add-plastic-info").slideToggle(400);
-        $(this).toggleClass('rotate'); // for button
+        $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".button-style > .general").click(function() {
+    $(".add-button-style > .general").click(function() {
         $(".add-general-info").slideToggle(400);
-        $(this).toggleClass('rotate'); // for button
+        $(this).toggleClass('rotate-button'); // for button
     });
 });
