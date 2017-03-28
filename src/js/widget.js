9de03a3c6842f2a3e3e2b81b7348cb380bfd700e
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-03-28 16:28:32 +0100

    Added comments to the code
    - The code is now well documented, which will act as part of the
    documentation required for the assessment

--- src/js/widget.js
+++ src/js/widget.js
@@ -1,31 +1,47 @@
 // TODO: Make the code more efficient, violates DRY
+
+/**
+ * 1. Document ready used to ensure the page has fully loaded
+ *
+ * 2. Each button is accessed via their unique id named after their material
+ *
+ * 3. All buttons are children of the .add-button-style class which applies
+ * the button style
+ *
+ * 4. Each click function initiates their own slideToggle which displays a
+ * new section, this section is targeted via the class 'add-x-info' where
+ * x denotes the unique material names
+ *
+ * 5. The rotate-button class is used in the toggleClass method to enable the
+ * rotation animation applied to each '+' button
+ */
 $(document).ready(function() {
-    $(".add-button-style > .paper").click(function() {
+    $(".add-button-style > #paper").click(function() {
         $(".add-paper-info").slideToggle(400);
         $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".add-button-style > .compost").click(function() {
+    $(".add-button-style > #compost").click(function() {
         $(".add-compost-info").slideToggle(400);
         $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".add-button-style > .glass").click(function() {
+    $(".add-button-style > #glass").click(function() {
         $(".add-glass-info").slideToggle(400);
         $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".add-button-style > .metal").click(function() {
+    $(".add-button-style > #metal").click(function() {
         $(".add-metal-info").slideToggle(400);
         $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".add-button-style > .plastic").click(function() {
+    $(".add-button-style > #plastic").click(function() {
         $(".add-plastic-info").slideToggle(400);
         $(this).toggleClass('rotate-button'); // for button
     });
 
-    $(".add-button-style > .general").click(function() {
+    $(".add-button-style > #general").click(function() {
         $(".add-general-info").slideToggle(400);
         $(this).toggleClass('rotate-button'); // for button
     });
