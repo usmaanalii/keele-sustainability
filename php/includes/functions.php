934a292cc2e7e9a5e455b79f74d00877d6fd0942
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-04-03 22:33:24 +0100

    .

--- php/includes/functions.php
+++ php/includes/functions.php
@@ -18,9 +18,9 @@ function dataget($type, $conn){
                 $tweight = $tweight/1000;
                 $tweight = round($tweight, 2);
                 echo "$tweight <span>kg</span>";
-            } else {echo "<h3>No data recorded for this month...yet!</h3>";}
-        } 
-    } else {echo "<h3>Something broke!</h3>";} 
+            } else {echo "<h1>No data recorded for this month...yet!</h1>";}
+        }
+    } else {echo "<h1>Something broke!</h1>";}
 }
 
 function datasend($type, $conn){
@@ -29,7 +29,7 @@ function datasend($type, $conn){
 		if ($stmt = $conn->prepare("INSERT INTO $type (weight, user_id, recorded)
 									VALUES (?, ?, NOW())")) {
 			$stmt->bind_param('ii', $record, $_SESSION['user_id']);
-			if($stmt->execute()) { 
+			if($stmt->execute()) {
 				echo "$record grams of $type was added to the database";
 			} else {echo $stmt->error;}
 		} else {echo $type;}
@@ -60,15 +60,15 @@ function graphget($conn, $type, $month, $year){
 
 
 function login($email, $password, $conn) {
-    // Using prepared statements means that SQL injection is not possible. 
-    if ($stmt = $conn->prepare("SELECT id, username, password 
+    // Using prepared statements means that SQL injection is not possible.
+    if ($stmt = $conn->prepare("SELECT id, username, password
         FROM members
        WHERE email = ?
         LIMIT 1")) {
         $stmt->bind_param('s', $email);  // Bind "$email" to parameter.
         $stmt->execute();    // Execute the prepared query.
         $stmt->store_result();
- 
+
         // get variables from result.
         $stmt->bind_result($user_id, $username, $db_password);
         $stmt->fetch();
@@ -102,7 +102,7 @@ if(!function_exists('hash_equals')) {
             $ret = 1;
         }
         $res = $known_string ^ $user_string;
-        
+
         for ($i = strlen($res) - 1; $i >= 0; --$i) {
             $ret |= ord($res[$i]);
         }
@@ -111,76 +111,76 @@ if(!function_exists('hash_equals')) {
 }
 
 function login_check($conn) {
-    // Check if all session variables are set 
-    if (isset($_SESSION['user_id'], 
-                        $_SESSION['username'], 
+    // Check if all session variables are set
+    if (isset($_SESSION['user_id'],
+                        $_SESSION['username'],
                         $_SESSION['login_string'])) {
- 
+
         $user_id = $_SESSION['user_id'];
         $login_string = $_SESSION['login_string'];
         $username = $_SESSION['username'];
- 
+
         // Get the user-agent string of the user.
         $user_browser = $_SERVER['HTTP_USER_AGENT'];
- 
+
         if ($stmt = $conn->prepare("SELECT password
-                                      FROM members 
+                                      FROM members
                                       WHERE id = ? LIMIT 1")) {
-            // Bind "$user_id" to parameter. 
+            // Bind "$user_id" to parameter.
             $stmt->bind_param('i', $user_id);
             $stmt->execute();   // Execute the prepared query.
             $stmt->store_result();
- 
+
             if ($stmt->num_rows == 1) {
                 // If the user exists get variables from result.
                 $stmt->bind_result($password);
                 $stmt->fetch();
                 $login_check = hash('sha512', $password . $user_browser);
- 
+
                 if (hash_equals($login_check, $login_string) ){
-                    // Logged In!!!! 
+                    // Logged In!!!!
                     return True;
                 } else {
-                    // Not logged in 
+                    // Not logged in
                     return False;
                 }
             } else {
-                // Not logged in 
+                // Not logged in
                 return False;
             }
         } else {
-            // Not logged in 
+            // Not logged in
             return False;
         }
     } else {
-        // Not logged in 
+        // Not logged in
         return False;
     }
 }
 
 function esc_url($url) {
- 
+
     if ('' == $url) {
         return $url;
     }
- 
+
     $url = preg_replace('|[^a-z0-9-~+_.?#=!&;,/:%@$\|*\'()\\x80-\\xff]|i', '', $url);
- 
+
     $strip = array('%0d', '%0a', '%0D', '%0A');
     $url = (string) $url;
- 
+
     $count = 1;
     while ($count) {
         $url = str_replace($strip, '', $url, $count);
     }
- 
+
     $url = str_replace(';//', '://', $url);
- 
+
     $url = htmlentities($url);
- 
+
     $url = str_replace('&amp;', '&#038;', $url);
     $url = str_replace("'", '&#039;', $url);
- 
+
     if ($url[0] !== '/') {
         // We're only interested in relative links from $_SERVER['PHP_SELF']
         return '';
