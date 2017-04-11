28efcad905809bdeb38bb8fe97829f97ca1760dc
Usmaan Ali <usmaanalii@hotmail.co.uk>
2017-04-11 19:05:35 +0100

    Cleaned up login javascript code

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-    $("#user-icon-image").click(function() {
-        $(".form-container").slideToggle(400);
-    });
+
+    /**
+     * loginClickEvent
+     *
+     * Used to reveal the login form when the user icon is clicked
+     *
+     * @return void [explained above]
+     */
+    var loginClickEvent = function() {
+        $("#user-icon-image").click(function() {
+            $(".form-container").slideToggle(400);
+        });
+    };
+
+    // funciton call
+    loginClickEvent();
 });
